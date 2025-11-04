5dacd1cdb9816d29df58c442e925c64973552fe1
ZacharyBuxton <151771297+ZacharyBuxton@users.noreply.github.com>
2025-11-04 15:28:00 -0500

    Added Selection sorting algorithm

--- Selectionsort.cc
+++ Selectionsort.cc
@@ -1,10 +1,18 @@
-/*
- * Selectionsort.cc
- *
- *  Created on: Nov 4, 2025
- *      Author: jooosh
- */
+// Zachary Buxton
+// Selection sorting algorithm
 
+#include <iostream>
+#include "myheaders.h"
+using namespace std;
 
+void selectionsort(long data[], int n) {
+    int least;
 
-
+    for (int i = 0; i < n - 1; i++) {
+        least = i;
+        for (int j = i + 1; j < n; j++) {
+            if (data[j] < data[least])
+                least = j;
+        }
+    }
+}
