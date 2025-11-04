a0ef0c331e7b4d2b848e87f4295f15bcd4de5501
Matt-russo11 <229957415+Matt-russo11@users.noreply.github.com>
2025-11-04 15:13:17 -0500

    commit bubble A sort

--- Asort.cc
+++ Asort.cc
@@ -1,4 +1,20 @@
 #include "myheaders.h"
 
-
+//Bubble sort : Matt
+//commit
 // Your ASort function(s) goes here ..
+void bubbleSort(){
+	bool swapped;
+	for (int i = 0; i < n - 1){
+		swapped = false;
+		for (int j = 0; j < n - 1 - i; j++){
+			if (arr[j] > arr[j + 1]){
+				long temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp; 
+				swapped = true;
+			}
+		}
+	}
+		
+}
