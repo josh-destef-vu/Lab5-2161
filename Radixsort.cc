fc95b62b41bc4dc35ef338a7ecc4e5f05b07b36e
josh-destef-vu <230369523+josh-destef-vu@users.noreply.github.com.>
2025-11-17 14:10:39 -0500

    radix fix again

--- Radixsort.cc
+++ Radixsort.cc
@@ -32,7 +32,7 @@ long getMin(long arr[], int n) {
 //go through and determine how many numbers are in each digit place
 void countSort(long arr[], int n, long exp) {
     long* output = new long[n]; //dynamic allocation because program breaks otherwise
-    int count[0] = {0};
+    int count[10] = {0};
 
     //Count total of each digit
     for (int i = 0; i < n; i++) {
